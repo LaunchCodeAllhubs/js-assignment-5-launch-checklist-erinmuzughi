6e69c9d028c2ebbe3eaf145554106d9f12cff53f
erinmuzughi <erinmuzughi@gmail.com>
2023-08-01 20:47:02 -0500

    finished assignment 5

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 
 window.addEventListener("load", function() {
     const form = document.getElementById("launchForm");
+    const list = document.getElementById("faultyItems");
+    list.style.visibility = "hidden";
     form.addEventListener("submit", function(event) {
        const pilotNameInput = document.querySelector("input[name=pilotName]");
        const copilotNameInput = document.querySelector("input[name=copilotName]");
        const fuelLevelInput = document.querySelector("input[name=fuelLevel]");
        const cargoMassInput = document.querySelector("input[name=cargoMass]");
-       const list = document.getElementById("faultyItems");
        // stop the form submission
        event.preventDefault();
        if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
@@ -33,7 +34,7 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
        selectedPlanet = pickPlanet(listedPlanets);
-       addDestinationInfo(document, selectedPlanet.name, selectedPlanet.diameter, selectedPlanet.star, selectedPlanet.distance, selectedPlanet.moons, selectedPlanet.image)
+       addDestinationInfo(document, selectedPlanet.name, selectedPlanet.diameter, selectedPlanet.star, selectedPlanet.distance, selectedPlanet.moons, selectedPlanet.image);
    })
    
 });
